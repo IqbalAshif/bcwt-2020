1558bea4d0d89dca0c38c84b0d89611104e4dc66
Ashif Iqbal <ashif.iqbal@metropolia.fi>
2020-11-13 12:14:51 +0200

    after class

--- app.js
+++ app.js
@@ -8,19 +8,28 @@ const catRoute = require('./routes/catRoute.js');
 const userRoute = require('./routes/userRoute.js');
 const authRoute = require('./routes/authRoute.js');
 const app = express();
-const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('uploads'));
 app.use('/thumbnails', express.static('thumbnails'));
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./production')(app, process.env.PORT);
+} else {
+  require('./localhost')(app, process.env.HTTPS_PORT, process.env.HTTP_PORT);
+}
+
 
 //routes
 app.use('/', rootRoute);
 app.use('/auth', authRoute);
-app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
-app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
+app.use('/cat', passport.authenticate('jwt', { session: false }), catRoute);
+app.use('/user', passport.authenticate('jwt', { session: false }), userRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+https
+  .createServer(options, app)
+  .listen(httpsPort, () => console.log(`And https on port ${httpsPort}`));
