8b9b224e915fcf74929275e5271ef37fda3e124a
Ashif Iqbal <ashif.iqbal@metropolia.fi>
2020-11-13 12:38:12 +0200

    error fixing in app.js

--- app.js
+++ app.js
@@ -29,7 +29,3 @@ app.use('/auth', authRoute);
 app.use('/cat', passport.authenticate('jwt', { session: false }), catRoute);
 app.use('/user', passport.authenticate('jwt', { session: false }), userRoute);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-https
-  .createServer(options, app)
-  .listen(httpsPort, () => console.log(`And https on port ${httpsPort}`));
